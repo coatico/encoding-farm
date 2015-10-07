c6783063948f3b5a9d5537e121be3fe6c70c3b41
r0450086 <arath@hotmail.be>
2015-10-07 16:59:55 +0200

    Sessiondto
    T316

--- manager/web-admin/src/main/java/manager/ui/ui/model/SessionDto.java
+++ manager/web-admin/src/main/java/manager/ui/ui/model/SessionDto.java
@@ -9,6 +9,58 @@ package manager.ui.ui.model;
  *
  * @author Arne
  */
-public class SessionDto {
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author lucs
+ */
+@Named
+@SessionScoped
+public class SessionDto implements Serializable {
+
+    private UserDto userDto;
+   long Start=0;
+    long stop=0;
     
+    public void startime(){
+       Start= System.currentTimeMillis();
+    }
+       
+    public void stoptime(){
+        stop=System.currentTimeMillis();
+        calworktime();
+    }
+    
+    public String calworktime(){
+        String a="";
+    if(stop==0){
+        a="";
+        }
+        if(stop!=0){
+       a= ((stop - Start) / 1000 +" seconden");
+        }
+        System.out.println(a);
+        System.out.println(Start);
+        System.out.println(stop);
+       return a;
+    } 
+    
+    @PostConstruct
+    public void init() {
+        System.out.println("SessionDto - init");
+        userDto = new UserDto();
+    }
+
+    public UserDto getUserDto() {
+        return userDto;
+    }
+
+    public void setUserDto(UserDto UserDto) {
+        this.userDto = UserDto;
+    }
+
 }
