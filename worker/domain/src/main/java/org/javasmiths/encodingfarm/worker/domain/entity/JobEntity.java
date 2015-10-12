937c96e7291065eb1b2a61d515220e79d81f5480
PieterDr <pieter_drijkoningen@hotmail.com>
2015-10-12 13:51:19 +0200

    T335 RequestEntity en JobEntity

--- worker/domain/src/main/java/org/javasmiths/encodingfarm/worker/domain/entity/JobEntity.java
+++ worker/domain/src/main/java/org/javasmiths/encodingfarm/worker/domain/entity/JobEntity.java
@@ -1,24 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.javasmiths.encodingfarm.worker.domain.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import org.javasmiths.encodingfarm.common.domain.entity.BaseTrackableEntity;
+
 /**
  *
  * @author Pieter
  */
 @Entity
-@Table(name = "ENTITIES")
-public class PathEntity extends BaseTrackableEntity {
+@Table(name = "JOBS")
+public class JobEntity extends BaseTrackableEntity {
     
     @Column
     private String video_path;
+    
+    @Column
+    private String status;
 
     public String getVideo_path() {
         return video_path;
@@ -26,5 +26,14 @@ public class PathEntity extends BaseTrackableEntity {
 
     public void setVideo_path(String video_path) {
         this.video_path = video_path;
-    }     
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }   
+    
 }
---
+++ worker/domain/src/main/java/org/javasmiths/encodingfarm/worker/domain/entity/RequestEntity.java
@@ -0,0 +1,28 @@
+
+package org.javasmiths.encodingfarm.worker.domain.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import org.javasmiths.encodingfarm.common.domain.entity.BaseTrackableEntity;
+
+/**
+ *
+ * @author Pieter
+ */
+@Entity
+@Table(name = "REQUEST")
+public class RequestEntity extends BaseTrackableEntity{
+    
+    @Column
+    private String video_path;
+
+    public String getVideo_path() {
+        return video_path;
+    }
+
+    public void setVideo_path(String video_path) {
+        this.video_path = video_path;
+    }
+    
+}
