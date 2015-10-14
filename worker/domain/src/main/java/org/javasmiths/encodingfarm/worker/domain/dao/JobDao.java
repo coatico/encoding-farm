ba6876bd2b2907332ad99873ef8378677db9ce73
PieterDr <pieter_drijkoningen@hotmail.com>
2015-10-14 14:28:46 +0200

    T342 JobDao(+Impl)

---
+++ worker/domain/src/main/java/org/javasmiths/encodingfarm/worker/domain/dao/JobDao.java
@@ -0,0 +1,16 @@
+
+package org.javasmiths.encodingfarm.worker.domain.dao;
+
+import java.util.List;
+import org.javasmiths.encodingfarm.worker.domain.entity.JobEntity;
+
+/**
+ *
+ * @author Pieter
+ */
+public interface JobDao {
+    
+    public void save(JobEntity dataEntity);
+    public List<JobEntity> listAll();
+
+}
