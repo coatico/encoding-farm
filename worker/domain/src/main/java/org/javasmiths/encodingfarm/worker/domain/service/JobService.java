cb7ee653c93823f92aeee16bf9e7ef79322bd1c5
QDockx <quintendockx@hotmail.com>
2015-10-15 14:28:43 +0200

    T348:
    JobDto, RequestDto, JobFacade,RequestFacade, JobFacadeImpl, RequestFacadeImpl,J obView, RequestView, RequestService,JobService, RequestServiceImpl, JobServiceImpl, Index.xhtml aangemaakt

--- worker/domain/src/main/java/org/javasmiths/encodingfarm/worker/domain/service/JobService.java
+++ worker/domain/src/main/java/org/javasmiths/encodingfarm/worker/domain/service/JobService.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.javasmiths.encodingfarm.worker.web.facade;
+package org.javasmiths.encodingfarm.worker.domain.service;
 
 /**
  *
- * @author User
+ * @author Quintendockx
  */
-public class WorkerFacade {
+public interface JobService {
     
 }
---
+++ worker/web/src/main/java/org/javasmiths/encodingfarm/worker/web/facade/RequestFacade.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.worker.web.facade;
+
+import java.util.List;
+import org.javasmiths.encodingfarm.worker.web.dto.RequestDto;
+
+/**
+ *
+ * @author Quintendockx
+ */
+public interface RequestFacade {
+    
+    
+    public void create(String name);
+
+	public List<RequestDto> listAll();
+}
