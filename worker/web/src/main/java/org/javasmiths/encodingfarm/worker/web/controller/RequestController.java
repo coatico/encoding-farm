198df547a23c561070b98599f1131f1e92bf7cfa
Jeroendw95 <jeroen.dw2@hotmail.com>
2015-10-15 18:42:52 +0200

    Encoding-farm worker RequestController gemaakt

--- worker/web/src/main/java/org/javasmiths/encodingfarm/worker/web/controller/RequestController.java
+++ worker/web/src/main/java/org/javasmiths/encodingfarm/worker/web/controller/RequestController.java
@@ -5,10 +5,28 @@
  */
 package org.javasmiths.encodingfarm.worker.web.controller;
 
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import org.javasmiths.encodingfarm.worker.web.dto.RequestDto;
+import org.javasmiths.encodingfarm.worker.web.facade.RequestFacade;
+
 /**
  *
  * @author Jeroen
  */
+@Path("requests")
 public class RequestController {
     
+  
+	@EJB
+	private RequestFacade facade;
+	
+	@GET
+	@Produces({"application/json", "application/xml"})
+	public List<RequestDto> listAll(){
+		return facade.listAll();
+	}
 }
