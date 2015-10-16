57e892675c716b59de28ef9209e359c8a93b5671
Gunther Van Kelst <gunthervankelst@hotmail.com>
2015-10-16 09:53:56 +0200

    Revert "T336"
    This reverts commit bfd02348c6fede036031468860ddceb617e8c12e.

---
+++ distributor/domain/src/main/java/org/javasmiths/encodingfarm/distributor/domain/service/VideoService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.distributor.domain.service;
+
+import java.util.List;
+import org.javasmiths.encodingfarm.distributor.domain.entity.VideoEntity;
+
+/**
+ *
+ * @author thomas
+ */
+public interface VideoService {
+
+    VideoEntity registerVideo(String name);
+
+    List<VideoEntity> listAll();
+}
