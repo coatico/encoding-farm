7f3decb0928db10ea1cbb2469473b71c03033dcf
r0450086 <arath@hotmail.be>
2015-10-16 16:59:29 +0200

    DAO
    T329

---
+++ manager/domain/src/main/java/org/javasmiths/encodingfarm/manager/domain/dao/WorkerDaoImpl.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.manager.domain.dao;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.javasmiths.encodingfarm.common.domain.dao.BaseDaoImpl;
+import org.javasmiths.encodingfarm.manager.domain.entity.WorkerEntity;
+
+/**
+ *
+ * @author Arne
+ */
+@Stateless
+public class WorkerDaoImpl extends BaseDaoImpl<WorkerEntity> implements WorkerDao{
+
+	@PersistenceContext(unitName = "manager-domainPU")
+	protected EntityManager em;
+	
+	
+	@Override
+	protected EntityManager em() {
+		return em;
+	}
+
+	@Override
+	protected Class<WorkerEntity> entityClass() {
+		return WorkerEntity.class;
+	}
+    
+
+}
