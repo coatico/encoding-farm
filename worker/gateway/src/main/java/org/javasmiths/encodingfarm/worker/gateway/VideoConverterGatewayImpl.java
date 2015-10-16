00ece106ced614c7c7d293fb4931f294554070f2
thomas <thomas@xbuntu>
2015-10-16 16:46:53 +0200

    VideoConverterGateway vanuit les

---
+++ worker/gateway/src/main/java/org/javasmiths/encodingfarm/worker/gateway/VideoConverterGatewayImpl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.worker.gateway;
+
+import org.javasmiths.encodingfarm.worker.domain.entity.RequestEntity;
+
+/**
+ *
+ * @author thomas
+ */
+public class VideoConverterGatewayImpl implements VideoConverterGateway{
+
+	@Override
+	public void convert(RequestEntity request) {
+		
+	}
+	
+}
