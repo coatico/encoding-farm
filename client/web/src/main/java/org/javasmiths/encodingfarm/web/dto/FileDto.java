ee716aa004dfb2ee67219b6d66ce983d3d1ab384
Stephan De Plucker <>
2015-10-16 12:20:08 +0200

    T364 De juiste strutuur aan de client-web gegeven zodat de interface kan worden gebouwd. Samengewerkt met Mukhran.

---
+++ client/web/src/main/java/org/javasmiths/encodingfarm/web/dto/FileDto.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.web.dto;
+
+/**
+ *
+ * @author thomas
+ */
+class FileDto {
+	private String id;
+	private String filePath;
+	private String fileDescription;
+	private String fileTitle;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getFileDescription() {
+		return fileDescription;
+	}
+
+	public void setFileDescription(String fileDescription) {
+		this.fileDescription = fileDescription;
+	}
+
+	public String getFileTitle() {
+		return fileTitle;
+	}
+
+	public void setFileTitle(String fileTitle) {
+		this.fileTitle = fileTitle;
+	}
+	
+	
+	
+}
