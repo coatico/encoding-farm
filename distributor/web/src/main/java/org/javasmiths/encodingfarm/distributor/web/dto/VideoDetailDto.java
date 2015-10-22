2ae3ea2a8a8bb00182df0fc603ab898812149e17
Raf <>
2015-10-22 13:39:08 +0200

    T336: CRUD fouten

---
+++ distributor/web/src/main/java/org/javasmiths/encodingfarm/distributor/web/dto/VideoDetailDto.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.distributor.web.dto;
+
+/**
+ *
+ * @author thomas
+ */
+public class VideoDetailDto extends VideoListDetailDto{
+	
+}
