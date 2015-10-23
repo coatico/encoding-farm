42343f9005c2e02b83a9d3220a10b4ff11363a25
thomas <thomas@xbuntu>
2015-10-23 14:24:14 +0200

    Revert "T336: CRUD fouten"
    This reverts commit b5a85626badaf409bba6e815ecd905931f974290.

--- distributor/domain/src/main/java/org/javasmiths/encodingfarm/distributor/domain/dao/VideoDaoImpl.java
+++ distributor/domain/src/main/java/org/javasmiths/encodingfarm/distributor/domain/dao/VideoDaoImpl.java
@@ -5,12 +5,9 @@
  */
 package org.javasmiths.encodingfarm.distributor.domain.dao;
 
-import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaQuery;
 import org.javasmiths.encodingfarm.common.domain.dao.BaseDaoImpl;
 import org.javasmiths.encodingfarm.distributor.domain.entity.VideoEntity;
 
@@ -19,7 +16,7 @@ import org.javasmiths.encodingfarm.distributor.domain.entity.VideoEntity;
  * @author thomas
  */
 @Stateless
-public class VideoDaoImpl extends BaseDaoImpl<VideoEntity> implements VideoDao{
+public class VideoDaoImpl extends BaseDaoImpl<VideoEntity> implements VideoDao {
 
     @PersistenceContext(unitName = "encoding_distributor")
     protected EntityManager em;
@@ -32,29 +29,6 @@ public class VideoDaoImpl extends BaseDaoImpl<VideoEntity> implements VideoDao{
     @Override
     protected Class<VideoEntity> entityClass() {
         return VideoEntity.class;
-    }
-	
-
-    public void save(VideoEntity videoEntity) {
-        em.merge(videoEntity);
-    }
-
-    public List<VideoEntity> listAll() {
-        CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
-        cq.select(cq.from(VideoEntity.class));
-        Query q = em.createQuery(cq);
-        return q.getResultList();
-    }
-
-    public VideoEntity findById(String id) {
-        if(id == null){
-            return null;
-        }
-        return em.find(VideoEntity.class, id);
-    }
-
-    public void deleteById(String id) {
-        em.remove(findById(id));
     }
 
 }
