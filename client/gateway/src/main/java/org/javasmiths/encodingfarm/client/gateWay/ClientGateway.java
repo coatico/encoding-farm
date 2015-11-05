14d07b2139a1da167b1aa127912258d5c6116213
Karlien <Karlien@10.0.0.115>
2015-11-05 15:51:06 +0100

    T429: opstart maken gateway (zelfde basis als encoding-sample-gateway)

--- client/gateway/src/main/java/org/javasmiths/encodingfarm/client/gateWay/ClientGateway.java
+++ client/gateway/src/main/java/org/javasmiths/encodingfarm/client/gateWay/ClientGateway.java
@@ -5,10 +5,34 @@
  */
 package org.javasmiths.encodingfarm.client.gateWay;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author thomas
  */
 public class ClientGateway {
 	
+    public void sample() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://api.openweathermap.org/data/2.5/weather?q=Landen");
+        Object o = target.request(MediaType.APPLICATION_JSON).get(Test.class);
+    }
+    
+
+    public static class Test {
+
+        private Long id;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+    }
 }
