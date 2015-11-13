cf4c8fe6dc09e2fdf3093cdc76b2838529e6f60e
lsteffens <luc.steffens@stecum.be>
2015-11-13 16:26:23 +0100

    Fixke

--- manager/gateway/src/main/java/org/javasmiths/encodingfarm/manager/gateway/JobGatewayImpl.java
+++ manager/gateway/src/main/java/org/javasmiths/encodingfarm/manager/gateway/JobGatewayImpl.java
@@ -5,39 +5,34 @@
  */
 package org.javasmiths.encodingfarm.manager.gateway;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.json.stream.JsonGenerationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import org.javasmiths.encodingfarm.manager.domain.entity.JobEntity;
-import org.json.JSONObject;
+import org.json.Test;
 
 /**
  *
  * @author Elias Serneels
  */
+
+
 public class JobGatewayImpl implements JobGateway {
 
     @Override
     public void convert(JobEntity job) {
-        
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080//json-sample/convert-job.json");
+        Object o = target.request(MediaType.APPLICATION_JSON).get(Test.class);
+        System.out.println("o");
     }
-    
+
     @PostConstruct
     public static void main(String[] args) {
         String string = "";
-        try {
+//        try {
 //                        // MANIER 1
 //			// temp JSON inputfile
 //                        // inlezen
@@ -75,30 +70,28 @@ public class JobGatewayImpl implements JobGateway {
 //				System.out.println("\nError while calling REST Service");
 //				System.out.println(e);
 //			}
-                        // MANIER 2
-                        ObjectMapper mapper = new ObjectMapper();
-                        try {
-                        
+        // MANIER 2
+//                        ObjectMapper mapper = new ObjectMapper();
+//                        try {
+
 			// JSON file die werd ingelezen omzetten in een JobEntity, werkt niet voor lists
 //			JobEntity job = mapper.readValue(new File("src\\main\\resources\\testJSON.json"), JobEntity.class);
 //			System.out.println(job.getJobTitle());
-                        
-                        //JSON file met lijst van objecten inlezen en omzetten in lijst van entities
-                        List<JobEntity> jobs = mapper.readValue(new File("src\\main\\resources\\testJSON.json"), new TypeReference<List<JobEntity>> () {});
-                        System.out.println(jobs.get(1).getJobTitle());
-		} catch (JsonGenerationException e) {
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
- 
-//			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        //JSON file met lijst van objecten inlezen en omzetten in lijst van entities
+//                        List<JobEntity> jobs = mapper.readValue(new File("src\\main\\resources\\testJSON.json"), new TypeReference<List<JobEntity>> () {});
+//                        System.out.println(jobs.get(1).getJobTitle());
+//		} catch (JsonGenerationException e) {
+//			e.printStackTrace();
+//		} catch (JsonMappingException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+// 
+////			br.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
     }
-
+}
