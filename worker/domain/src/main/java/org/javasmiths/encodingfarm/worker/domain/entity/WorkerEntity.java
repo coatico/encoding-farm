832c1053ea507015b9058e047b079215baffcaeb
Jeroendw95 <jeroen.dw2@hotmail.com>
2015-11-18 21:47:06 +0100

    encoding-farm T445 Worker CRUD aanmaken Entity en Service

---
+++ worker/domain/src/main/java/org/javasmiths/encodingfarm/worker/domain/entity/WorkerEntity.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.worker.domain.entity;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.javasmiths.encodingfarm.common.domain.entity.BaseTrackableEntity;
+
+/**
+ *
+ * @author Jeroen
+ */
+
+@Entity
+@Table(name = "WORKER")
+public class WorkerEntity extends BaseTrackableEntity {
+
+    
+@OneToMany(targetEntity = JobEntity.class, fetch = FetchType.LAZY, mappedBy = "request")
+private List<JobEntity> jobs;
+
+
+    @Column
+    private String url;
+
+    public List<JobEntity> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<JobEntity> jobs) {
+        this.jobs = jobs;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    
+    
+}
