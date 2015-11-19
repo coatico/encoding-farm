bf527907f09bd0e4cb1e705414faec7a396dda0e
Jeroendw95 <jeroen.dw2@hotmail.com>
2015-11-19 17:53:15 +0100

    encoding-farm T445 Worker CRUD aanmaken worker controller en Dto

---
+++ worker/web/src/main/java/org/javasmiths/encodingfarm/worker/web/dto/WorkerDto.java
@@ -0,0 +1,34 @@
+package org.javasmiths.encodingfarm.worker.web.dto;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Quintendockx
+ */
+public class WorkerDto {
+
+    private String id;
+    private String url;
+    
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+   
+
+}
