c518eb8621afbc9a6028913df5de160fd2fa14aa
PieterDr <pieter_drijkoningen@hotmail.com>
2015-11-20 18:38:00 +0100

    Test Gateway uit les 20/11

---
+++ worker/gateway/src/main/java/org/javasmiths/encodingfarm/worker/gateway/TestDto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.worker.gateway;
+
+/**
+ *
+ * @author Pieter
+ */
+public class TestDto {
+        private String status = "test";
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+    
+}
---
+++ worker/gateway/src/test/java/org/javasmiths/encodingfarm/worker/gateway/TestJobGatewayTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.worker.gateway;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pieter
+ */
+public class TestJobGatewayTest {
+
+    /**
+     * Test of test method, of class TestJobGateway.
+     */
+    @Test
+    public void testTest() {
+        TestJobGateway t = new TestJobGateway();
+        t.test();
+    }
+    
+}
