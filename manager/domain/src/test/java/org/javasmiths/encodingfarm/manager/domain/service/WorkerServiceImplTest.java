fbba63696d8f594e584b0fb9d84ee4661ba972fb
Olivier <>
2015-11-20 17:05:54 +0100

    Pushen voorbeeld JUnit test
    Olivier

---
+++ manager/domain/src/test/java/org/javasmiths/encodingfarm/manager/domain/service/WorkerServiceImplTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasmiths.encodingfarm.manager.domain.service;
+
+import java.util.List;
+import org.javasmiths.encodingfarm.manager.domain.entity.WorkerEntity;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ *
+ * @author thomas
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class WorkerServiceImplTest {
+	
+	public WorkerServiceImplTest() {
+	}
+
+	@Test
+	public void testRegisterWorker() throws Exception {
+	}
+
+	@Test
+	public void testEditWorker() throws Exception {
+	}
+
+	@Test
+	public void testDeleteWorker() throws Exception {
+	}
+
+	@Test
+	public void testListAll() throws Exception {
+	}
+	
+}
