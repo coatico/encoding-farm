6040211280117aacca3876333edc34f65c780a21
r0450086 <arath@hotmail.be>
2015-11-26 17:40:03 +0100

    T450 - Contoller

--- manager/web-admin/src/main/java/org/javasmiths/encodingfarm/manager/webadmin/controller/WorkerController.java
+++ manager/web-admin/src/main/java/org/javasmiths/encodingfarm/manager/webadmin/controller/WorkerController.java
@@ -7,9 +7,13 @@ package org.javasmiths.encodingfarm.manager.webadmin.controller;
 
 import java.util.List;
 import javax.ejb.EJB;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import org.javasmiths.encodingfarm.manager.webadmin.facade.WorkerFacade;
 import org.javasmiths.encodingfarm.manager.webadmin.model.WorkerListDetailDto;
 
@@ -19,15 +23,24 @@ import org.javasmiths.encodingfarm.manager.webadmin.model.WorkerListDetailDto;
  */
 @Path("workers")
 public class WorkerController {
-	
-	@EJB
-	private WorkerFacade facade;
-	
-	@GET
-	@Produces({"application/json", "application/xml"})
-	public List<WorkerListDetailDto> listAll(){
-		return facade.listAll();
-	}
-	
-	
+
+    @EJB
+    private WorkerFacade facade;
+
+    @GET
+    @Produces({"application/json", "application/xml"})
+    public List<WorkerListDetailDto> listAll() {
+        return facade.listAll();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String createJob(JsonObject json) {
+        String name = json.getString("name");
+        Boolean status = json.getBoolean("status");
+        String url = json.getString("url");
+        facade.create(name, status, url);
+        return "created job with name: " + name + ", status " + status + " and url " + url;
+    }
+
 }
