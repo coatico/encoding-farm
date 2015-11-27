c28a3c12af7e66250f9fde659c8007d8f490e07b
Rafportael <rafportael@hotmail.com>
2015-11-27 08:27:44 +0100

    uitlijning op videopagina.xhtml

--- distributor/web/src/main/java/org/javasmiths/encodingfarm/distributor/web/config/ApplicationConfig.java
+++ distributor/web/src/main/java/org/javasmiths/encodingfarm/distributor/web/config/ApplicationConfig.java
@@ -20,8 +20,9 @@ public class ApplicationConfig extends Application {
      * out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(org.javasmiths.encodingfarm.distributor.domain.entity.InitDbStartup.class);
         resources.add(org.javasmiths.encodingfarm.distributor.web.controller.MockClientController.class);
-        resources.add(org.javasmiths.encodingfarm.distributor.web.controller.VideoController.class);
+		resources.add(org.javasmiths.encodingfarm.distributor.web.controller.VideoController.class);
     }
 
 }
